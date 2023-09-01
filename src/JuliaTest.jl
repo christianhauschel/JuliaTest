fd25e618c5960763bc72f1ebd8790e6d019c0fbc
Christian <hacs@zhaw.ch>
2023-09-01 08:59:50 +0200

    updates

--- src/JuliaTest.jl
+++ src/JuliaTest.jl
@@ -3,11 +3,11 @@ module JuliaTest
 using Revise
 
 include("./Submodule1/Submodule1.jl")
-using .Submodule1: greet1, square
+using .Submodule1
 export greet1, square
 
 include("./Submodule2/Submodule2.jl")
-using .Submodule2: greet2
+using .Submodule2
 export greet2
 
 
